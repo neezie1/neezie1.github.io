e578800307f4300e91d8e65223b4b289e20a976d
Neezie <154235122+neezie1@users.noreply.github.com>
2024-09-06 21:55:05 +0100

    Create file.lua

---
+++ file.lua
@@ -0,0 +1,26 @@
+getgenv().KiTTYWARE_ColorWar = {
+    Farm_Zones = true,
+    Farming_Misc = {
+        Pets_Per_Breakable = 3,
+        Auto_Pickups = true,
+        Inf_Pet_Speed = true,
+
+        Event_Specific = {
+            Color_Gifts = {Enabled = false, Minimum_Buckets = 999},
+            Clan_Points = {Enabled = false, Minimum_Buckets = 999},
+            Stay_Teams = {Enabled = false, Only_Winning = false},
+        },
+    },
+    Hatch_Eggs = true,
+    Hatching_Misc = {
+        discord_UserID = "1202699896235360386",
+        hugeNotif_Webhook = "https://discord.com/api/webhooks/1281718701158043732/kcnFg9JFXjjnA4J6e5LrA6ov6eDknvcPOiTXQfllhBWL7mhTP-LVUoibUyXmntU2W0mA",
+        hugeNotif_Enabled = true,
+        autoMail_User = "",
+        autoMail_Enabled = false,
+    },
+    StatsGui = true,
+    Fps_Boost1 = true,
+    Fps_Boost2 = true,
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/38e4778402a1d51414b40ab60274c5ed.lua"))()
